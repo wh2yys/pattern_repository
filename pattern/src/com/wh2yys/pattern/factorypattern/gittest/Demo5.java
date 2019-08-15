c13d26b4b8e67a1a7a059aaca19bfd59e94dd455
wh2yys <wh2yys@126.com>
2019-08-15 13:57:48 +0800

    dev分支 在建分支测试提交

---
+++ pattern/src/com/wh2yys/pattern/factorypattern/gittest/Demo5.java
@@ -0,0 +1,22 @@
+package com.wh2yys.pattern.factorypattern.gittest;
+
+public class Demo5 {
+    public static void main(String[] args) {
+        System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");
+
+    }
+}
