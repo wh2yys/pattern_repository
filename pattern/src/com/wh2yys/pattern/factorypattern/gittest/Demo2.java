443f1f0834f409a7d2fc624cce6edb09e814dfa0
wh2yys <wh2yys@126.com>
2019-08-15 13:02:59 +0800

    test1分支提交测试

---
+++ pattern/src/com/wh2yys/pattern/factorypattern/gittest/Demo2.java
@@ -0,0 +1,11 @@
+package com.wh2yys.pattern.factorypattern.gittest;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        System.out.println("test1");
+        System.out.println("test2");
+        System.out.println("test3");
+        System.out.println("test4");
+        System.out.println("test5");
+    }
+}
